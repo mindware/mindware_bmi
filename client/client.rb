28ce7d59699595bb4f16e54754aa5e102b19d156
Andrés Colón <levipr@gmail.com>
2013-03-02 17:46:21 -0400

    Parsing eSense data

--- client/client.rb
+++ client/client.rb
@@ -3,12 +3,19 @@ module Client
 
 	def post_init
 		puts "Loading Client Connector..."
-		puts "Switching from binary input..."
+		puts "Requesting switch from binary input..."
+		# Request JSON output
 		send_data '{"enableRawOutput": true, "format": "Json"}'
 	end
 
 	def receive_data(data)
-		puts data if data.include? "eSense"
+		#puts data if data.include? "eSense"
+		if data.include? "eSense"
+			parsed = JSON.parse(data)
+			print "Attention: #{parsed["attention"]}" if !parsed["attention"].nil?
+			print "Meditation: #{parsed["meditation"}" if !parsed["meditation"].nil?
+			puts ""
+		end
 	end
 	
 	def unbind
